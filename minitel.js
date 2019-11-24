e95d976da1dd71d9f5c81a479b16531620cf53e1
cth103 <cthdev@email.com>
2019-11-24 20:09:13 +0100

    bordel

--- minitel.js
+++ minitel.js
@@ -6,7 +6,7 @@ par jiktim
 const EventEmitter = require("events");
 const serial = require("serialport"); 
 const noEventHandler =  { 	on: ()=>{},
-							emit: ()=>{} 
+						    emit: ()=>{} 
 						};
 
 let serialConnection = noEventHandler;
