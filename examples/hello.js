dd678d3c2d9a64cdab8e808234b3a774eaba4483
cth103 <cthdev@email.com>
2019-11-24 22:36:00 +0100

    Update hello.js

--- examples/hello.js
+++ examples/hello.js
@@ -2,3 +2,10 @@
 minitel.js
 example hello world!
 */
+
+const minitel = require("minitel");
+let server = new minitel("/dev/ttyUSB0", false); // Initialize the server on the USB device
+
+server.on("ready", () => { // This is usually not needed.
+  server.print("Hello, world!\nHello, Minitel!\n\nFrom: minitel.js");
+});
