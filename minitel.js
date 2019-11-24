99e7f2eff7943269baba0532f3e71cb5dcbf0ff8
cth103 <cthdev@email.com>
2019-11-24 19:33:14 +0100

    MAJ 1

--- minitel.js
+++ minitel.js
@@ -3,9 +3,15 @@ Minitel.js
 par jiktim
 */
 
+let serial = require("serialport"); 
+let serialConnection = null;
+
 class Minitel {
-    constructor(path, isHighSpeed) {
+    constructor(path="/dev/ttyUSB0", isHighSpeed=false) {
         this.path = path; // du type "/dev/ttyUSB0"
         this.isHighSpeed = isHighSpeed; // Minitel 2+ seulement !
+        serialConnection = new serial(path, {
+            baudRate: isHighSpeed ? 9600 : 1200;
+        });
     }
 }
