452d9dc2afe8639b1c91ec86b8be14ceedbeb3cd
cth103 <cthdev@email.com>
2019-11-24 20:51:37 +0100

    Format!
    https://github.com/cquest/pynitel/blob/master/pynitel.py#L415

--- minitel.js
+++ minitel.js
@@ -23,6 +23,7 @@ class Minitel extends EventEmitter {
 					*/
 					baudRate: (isHighSpeed ? 9600 : 1200),
 				});
+				
 				serialConnection.on("open", () => { 
 					this.hasOpened = true;
 					this.emit("ready", true);
@@ -43,8 +44,55 @@ class Minitel extends EventEmitter {
 		}
 		
 		_format(texte) {
-			// TODO: faire un truc
-			return texte;
+			let _tempTexte = texte;
+			/* 
+			Source:
+			https://github.com/cquest/pynitel/blob/master/pynitel.py#L415 
+			*/
+			
+			// Conversion des caractères accentués (cf STUM p 103)
+			_tempTexte = _tempTexte.replace('à', '\x19\x41a')
+			_tempTexte = _tempTexte.replace('â', '\x19\x43a')
+			_tempTexte = _tempTexte.replace('ä', '\x19\x48a')
+			_tempTexte = _tempTexte.replace('è', '\x19\x41e')
+			_tempTexte = _tempTexte.replace('é', '\x19\x42e')
+			_tempTexte = _tempTexte.replace('ê', '\x19\x43e')
+			_tempTexte = _tempTexte.replace('ë', '\x19\x48e')
+			_tempTexte = _tempTexte.replace('î', '\x19\x43i')
+			_tempTexte = _tempTexte.replace('ï', '\x19\x48i')
+			_tempTexte = _tempTexte.replace('ô', '\x19\x43o')
+			_tempTexte = _tempTexte.replace('ö', '\x19\x48o')
+			_tempTexte = _tempTexte.replace('ù', '\x19\x43u')
+			_tempTexte = _tempTexte.replace('û', '\x19\x43u')
+			_tempTexte = _tempTexte.replace('ü', '\x19\x48u')
+			_tempTexte = _tempTexte.replace('ç', '\x19\x4Bc')
+			_tempTexte = _tempTexte.replace('°', '\x19\x30')
+			_tempTexte = _tempTexte.replace('£', '\x19\x23')
+			_tempTexte = _tempTexte.replace('Œ', '\x19\x6A').replace('œ', '\x19\x7A')
+			_tempTexte = _tempTexte.replace('ß', '\x19\x7B')
+			
+			// Caractères spéciaux
+			_tempTexte = _tempTexte.replace('¼', '\x19\x3C')
+			_tempTexte = _tempTexte.replace('½', '\x19\x3D')
+			_tempTexte = _tempTexte.replace('¾', '\x19\x3E')
+			_tempTexte = _tempTexte.replace('←', '\x19\x2C')
+			_tempTexte = _tempTexte.replace('↑', '\x19\x2D')
+			_tempTexte = _tempTexte.replace('→', '\x19\x2E')
+			_tempTexte = _tempTexte.replace('↓', '\x19\x2F')
+			_tempTexte = _tempTexte.replace('̶', '\x60')
+			_tempTexte = _tempTexte.replace('|', '\x7C')
+			
+			// Caractères accentués inexistants sur Minitel
+			_tempTexte = _tempTexte.replace('À', 'A').replace('Â', 'A').replace('Ä', 'A')
+			_tempTexte = _tempTexte.replace('È', 'E').replace('É', 'E')
+			_tempTexte = _tempTexte.replace('Ê', 'E').replace('Ë', 'E')
+			_tempTexte = _tempTexte.replace('Ï', 'I').replace('Î', 'I')
+			_tempTexte = _tempTexte.replace('Ô', 'O').replace('Ö', 'O')
+			_tempTexte = _tempTexte.replace('Ù', 'U').replace('Û', 'U').replace('Ü', 'U')
+			_tempTexte = _tempTexte.replace('Ç', 'C')
+			
+			return _tempTexte;
+			_tempTexte = ""; // Good Practice :)
 		}
 	
 		_print(text) {
