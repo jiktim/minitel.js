ba73fae8a8866b1a41df2a3ca478c4f217839038
cth103 <cthdev@email.com>
2019-11-24 19:35:31 +0100

    Ajouter une source

--- minitel.js
+++ minitel.js
@@ -11,6 +11,10 @@ class Minitel {
         this.path = path; // du type "/dev/ttyUSB0"
         this.isHighSpeed = isHighSpeed; // Minitel 2+ seulement !
         serialConnection = new serial(path, {
+            /*
+            Selon:
+            http://minitel.cquest.org/musee/minitel/documentation-utilisateurs/Mode%20d%27emploi%20Minitel%202%20philips.pdf
+            */
             baudRate: isHighSpeed ? 9600 : 1200;
         });
     }
