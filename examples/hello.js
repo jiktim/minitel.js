ccb286f9cde0ee2d83bd4f617b07f051b54fea51
cth103 <cthdev@email.com>
2019-11-25 14:42:42 +0100

    Update hello.js

--- examples/hello.js
+++ examples/hello.js
@@ -9,5 +9,5 @@ let server = new minitel("/dev/ttyUSB0", false);
 
 // This is usually not needed.
 server.on("ready", () => {
-  server.print("Hello, world!\nHello, Minitel!\n\nFrom: minitel.js");
+  server.print("Hello, world!\nHello, Minitel!\n\nFrom: jiktim's minitel.js");
 });
